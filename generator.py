e87e04e17d42f1c98b14c31796404dc9ef8f0f48
Abbey Truong <57568294+attruong00@users.noreply.github>
2020-09-08 14:21:02 -0500

    delete code to zip locally

--- generator.py
+++ generator.py
@@ -1,43 +1,5 @@
 import gen_world_ca
-import os
-from os.path import basename
-from zipfile import ZipFile
 import datetime
-
-
-def is_world(s):
-  return '.world' in s
-
-
-def is_grid(s):
-  return 'grid_' in s
-
-
-def is_path(s):
-  return 'path_' in s
-
-
-def is_difficulty(s):
-  return 'difficulties_' in s
-
-
-def is_pgm(s):
-  return '.pgm' in s
-
-def is_yaml(s):
-  return '.yaml' in s
-
-
-def zipFilesInDir(dirName, zipFileName, filter):
-  # create ZipFile object
-  with ZipFile(zipFileName, 'w') as zipObj:
-    # iterate over files in directory
-    for folderName, subfolders, filenames in os.walk(dirName):
-      for filename in filenames:
-        if filter(filename):
-          # create complete filepath of file in directory
-          filePath = os.path.join(folderName, filename)
-          zipObj.write(filePath, basename(filePath))
 
 
 # generates dataset of 300 worlds
@@ -57,20 +19,6 @@ def main():
         if result:
           param_counter += 1
           total_counter += 1
-
-  """
-  # get current directory
-  curr_dir = os.getcwd()
-  print(curr_dir)
-
-  # zip files
-  zipFilesInDir(curr_dir, 'data/world_files/Generated Worlds.zip', is_world)
-  zipFilesInDir(curr_dir, 'data/grid_files/Generated Grids.zip', is_grid)
-  zipFilesInDir(curr_dir, 'data/path_files/Generated Paths.zip', is_path)
-  zipFilesInDir(curr_dir, 'data/diff_files/Generated Difficulties.zip', is_difficulty)
-  zipFilesInDir(curr_dir, 'data/pgm_files/Generated Pgms.zip', is_pgm)
-  zipFilesInDir(curr_dir, 'data/yaml_files/Generated Yamls.zip', is_yaml)
-  """
 
 
 if __name__ == "__main__":
