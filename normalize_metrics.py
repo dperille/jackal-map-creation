3dcca27a2381bc840874a2f8af2812832757a060
Abbey Truong <57568294+attruong00@users.noreply.github>
2020-09-22 11:24:38 -0500

    update var names

--- normalize_metrics.py
+++ normalize_metrics.py
@@ -17,18 +17,21 @@ def load_metrics(metrics_dir, num_files):
 
 
 # all_metrics is a 2D NumPy array made from appending all difficulty metrics arrays
+# all_means: (1x5) NumPy array with means for each metric
+# all_stds: (1x5) NumPy array with standard deviations for each metric
 def calc_stats(all_metrics):
   all_means = np.mean(all_metrics, axis=0)
   all_stds = np.std(all_metrics, axis=0)
   return all_means, all_stds
 
-# metrics_arr is the array with all metrics (300x5)
+# metrics_arr is the array with all metrics (nx5)
 # means is the array with means for each metric (1x5)
-# stds is the array with standard devs for each metrics (1x5)
+# stds is the array with standard deviationss for each metric (1x5)
 # all metrics are modified in place
 def normalize_all(metrics_arr, means, stds):
   for datapt in range(len(metrics_arr)):
     for metric in range(5):
+      # normalized metric = (val - mean) / std
       metrics_arr[datapt][metric] -= means[metric]
       metrics_arr[datapt][metric] /= stds[metric]
   
@@ -43,24 +46,27 @@ def save_norm_metrics(dir_name, metrics_arr):
 
 # loads all metrics files, normalizes metrics, and saves to directory
 def main():
+  num_files = 300
+  metrics_dir = 'test_data/metrics_files/'
+  norm_metrics_dir = 'test_data/norm_metrics_files/'
+  
   # load files
-  phys_arr = load_metrics('phys_data/metrics_files/', 10)
-  dataset_arr = load_metrics('dataset/metrics_files/', 300)
+  dataset_arr = load_metrics(metrics_dir, num_files)
 
   # print min/max
   print('MINIMUMS')
-  print(np.amin(phys_arr, axis=0))
+  print(np.amin(dataset_arr, axis=0))
   print('MAXIMUMS')
-  print(np.amax(phys_arr, axis=0))
+  print(np.amax(dataset_arr, axis=0))
 
   # get the means and standard devs
   means, stds = calc_stats(dataset_arr)
 
   # normalize metrics
-  metrics_arr = normalize_all(phys_arr, means, stds)
+  dataset_arr = normalize_all(dataset_arr, means, stds)
 
   # save to files
-  save_norm_metrics('phys_data/norm_metrics_files/', phys_arr)
+  save_norm_metrics(norm_metrics_dir, dataset_arr)
 
 if __name__ == "__main__":
   main()
