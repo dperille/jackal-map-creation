0685c5f4883923fc7019f2ad7048afaa71a32dc4
Abbey Truong <57568294+attruong00@users.noreply.github>
2020-09-22 11:08:37 -0500

    update var names

--- c_space.py
+++ c_space.py
@@ -1,15 +1,18 @@
 import numpy as np
 from gen_world_ca import JackalMap
 
-jackal_radius = 2
 
+jackal_radius = 2 # Jackal takes up 2 cells in each direction in addition to center (5x5)
+num_files = 300
+
+# creates C-space files from given occupancy grids and robot radius
 def create_cspace_files(obs_map_dir, num_files, cspace_dir, robot_radius):
     for i in range(num_files):
         output_file = cspace_dir + 'cspace_%d.npy' % i
         input_file = obs_map_dir + 'grid_%d.npy' % i
 
         obs_map = np.load(input_file)
-        jm = JackalMap(obs_map, radius)
+        jm = JackalMap(obs_map, robot_radius)
         cspace_grid = jm.get_map()
 
         # save c-space
@@ -17,4 +20,4 @@ def create_cspace_files(obs_map_dir, num_files, cspace_dir, robot_radius):
         np.save(output_file, cspace_grid)
 
 if __name__ == "__main__":
-    create_cspace_files('phys_data/grid_files/', 10, 'phys_data/cspace_files/', jackal_radius)
+    create_cspace_files('test_data/grid_files/', num_files, 'test_data/cspace_files/', jackal_radius)
